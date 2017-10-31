beb431e95bd7fa30b74bc9fe06c5afa12a4f7cbf
Duc Phan <phduc.phanhongduc@gmail.com>
2017-10-31 03:35:46 +0200

    implement repository and migration database

--- TeduShop.Data/Repositories/ProductRepository.cs
+++ TeduShop.Data/Repositories/ProductRepository.cs
@@ -2,8 +2,8 @@
 using TeduShop.Model.Models;
 
 namespace TeduShop.Data.Repositories
-{ 
-    public interface IProductRepository : IRepository<Product>
+{
+    public interface IProductRepository
     {
     }
 
